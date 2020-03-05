60f346f5a449b46e23a5938c3a959f9878f373be
keita <onini21@gmail.com>
2020-03-05 09:19:29 +0000

    Add account activation

--- db/schema.rb
+++ db/schema.rb
@@ -19,7 +19,9 @@ ActiveRecord::Schema.define(version: 2020_03_04_090552) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "remember_digest"
-    t.boolean "admin", default: false
+    t.string "activation_digest"
+    t.boolean "activated", default: false
+    t.datetime "activated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 

251a6f7b2c400b24b5fd8583abfa74b5251a6e4e
keita <onini21@gmail.com>
2020-03-09 11:21:30 +0000

    フォロー機能追加

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,9 +1,18 @@
 require 'test_helper'
 
-class SessionsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get login_path
-    assert_response :success
+class RelationshipsControllerTest < ActionDispatch::IntegrationTest
+
+  test "create should require logged-in user" do
+    assert_no_difference 'Relationship.count' do
+      post relationships_path
+    end
+    assert_redirected_to login_url
   end
 
+  test "destroy should require logged-in user" do
+    assert_no_difference 'Relationship.count' do
+      delete relationship_path(relationships(:one))
+    end
+    assert_redirected_to login_url
+  end
 end
